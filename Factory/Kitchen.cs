190cb920922a8f12f13527d2a386bc8a5772ed43
Ben Wolf <benjamin.wolf.de@googlemail.com>
2017-09-01 17:46:59 +0200

    Our Kitchen now owns an oven!

--- Factory/Kitchen.cs
+++ Factory/Kitchen.cs
@@ -1,13 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using CookieJar.Enum;
 using CookieJar.Objects;
 
-namespace CookieJar
+namespace CookieJar.Factory
 {
     /// <summary>
     /// The place where our story will take place.
     /// </summary>
     public static class Kitchen
     {
+        /// <summary>
+        /// This is it. Our cookie jar, the holy grail of cookies.
+        /// </summary>
         private static readonly Jar _almightyCookieJar = new Jar();
 
         /// <summary>
@@ -16,5 +21,12 @@ namespace CookieJar
         /// <returns>The one and only cookie jar.</returns>
         public static Jar GrabCookieJar() => _almightyCookieJar;
 
+        /// <summary>
+        /// Bakes the cookies in the oven. 
+        /// </summary>
+        /// <returns>Frehsly baked cookies!</returns>
+        /// <param name="aType">Cookie type.</param>
+        /// <param name="aAmount">Amount of cookies we want.i</param>
+        public static IList<Cookie> BakeCookiesInOven(CookieType aType, int aAmount) => Oven.Bake(aType, aAmount);
     }
 }
