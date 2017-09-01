e0be719425d17441efc9d3d76bbf4014d2aeb0ab
Ben Wolf <benjamin.wolf.de@googlemail.com>
2017-09-01 17:47:09 +0200

    The scene is set...

--- Program.cs
+++ Program.cs
@@ -1,12 +1,72 @@
 ﻿using System;
+using System.Threading.Tasks;
+using CookieJar.Factory;
 
 namespace CookieJar
 {
-    class MainClass
+    /// <summary>
+    /// Here's where all the action happens. Cats, CookieMonster and good ol' Grandma.
+    /// </summary>
+    public static class MainClass
     {
-        public static void Main(string[] args)
+        /// <summary>
+        /// Let's go, bake some cookies!
+        /// </summary>
+        /// <param name="args">Who nees arguments.</param>
+        public static async void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // someone baked some cookies already. Very kind.
+            Kitchen.GrabCookieJar().StoreCookies(Kitchen.BakeCookiesInOven(Enum.CookieType.DoubleChocolate, 7));
+
+
+            var curiousCat = Task.Run(async () =>
+            {
+                // the cat sleeps for two seconds, before it tries to steal a cookie.
+                await Task.Delay(2000);
+
+                var jar = Kitchen.GrabCookieJar();
+                var cookie = jar.TakeCookies(1);
+
+                if (cookie.Count == 1)
+                {
+                    Console.WriteLine("Cat: I am happy! :) om nom");
+                }
+                else
+                {
+                    Console.WriteLine("Cat: I am so sad, no cookie!");
+                }
+            });
+
+            var theCookieMonster = Task.Run(async () =>
+            {
+                // we do not know exactly what the monster does.
+                await Task.Delay(new Random().Next(5000));
+
+				// but when it awakes, it is hungry. 
+				var cookies = Kitchen.GrabCookieJar().TakeCookies(6);
+
+                if (cookies.Count == 0) {
+                    Console.WriteLine("Monster: RAWWRRRRR!");
+                }
+                else 
+                {
+                    foreach (var cookie in cookies)
+                    {
+                        Console.WriteLine("Monster: Omnomnom.");
+                    }
+                }
+            });
+
+            var goodOlGrandMa = Task.Run(async () =>
+            {
+                // grand ma is slow
+                await Task.Delay(8000);
+
+                var cookies = Kitchen.BakeCookiesInOven(Enum.CookieType.TripleChocolate, 10);
+                Kitchen.GrabCookieJar().StoreCookies(cookies);
+            });
+
+            await Task.WhenAll(new[] {curiousCat, theCookieMonster, goodOlGrandMa});
         }
     }
 }
