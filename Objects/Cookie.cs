d5733c0c60d52f2cc693801fd18434f578b49728
Ben Wolf <benjamin.wolf.de@googlemail.com>
2017-09-01 17:46:10 +0200

    Cookies need a type!

--- Objects/Cookie.cs
+++ Objects/Cookie.cs
@@ -1,10 +1,26 @@
 ﻿using System;
+using CookieJar.Enum;
+
 namespace CookieJar.Objects
 {
+
+    /// <summary>
+    /// Obviously, this thing is a cookie. 
+    /// </summary>
     public class Cookie
     {
-        public Cookie()
+        /// <summary>
+        /// Each cookie has to have a type, don't you think?
+        /// </summary>
+        private readonly CookieType _type;
+
+        /// <summary>
+        /// Create new cookie of specific type <paramref name="aType"/>.
+        /// </summary>
+        /// <param name="aType">The type of the cookie.</param>
+        public Cookie(CookieType aType)
         {
+            _type = aType;
         }
     }
 }
