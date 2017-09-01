da101d6afae843be95d1db47b7657a86fc114b43
Ben Wolf <benjamin.wolf.de@googlemail.com>
2017-09-01 17:45:51 +0200

    We need a fully functional oven to bake cookies.

--- Factory/Oven.cs
+++ Factory/Oven.cs
@@ -1,10 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using CookieJar.Enum;
+using CookieJar.Objects;
+
 namespace CookieJar.Factory
 {
-    public class Oven
+    /// <summary>
+    /// This oven is used to create new cookies.
+    /// </summary>
+    internal static class Oven
     {
-        public Oven()
+        /// <summary>
+        /// Aw, so many fresh cookies! 
+        /// </summary>
+        /// <returns>All the cookies, hopefully none is burnt.</returns>
+        /// <param name="aType">Chocolate cookie type!</param>
+        /// <param name="aAmount">We want only so many...</param>
+        public static IList<Cookie> Bake(CookieType aType, int aAmount)
         {
+            var newCookies = new List<Cookie>(aAmount);
+            for (int i = 0; i < aAmount; i++)
+            {
+                newCookies.Add(new Cookie(aType));
+            }
+
+            return newCookies;
         }
     }
 }
