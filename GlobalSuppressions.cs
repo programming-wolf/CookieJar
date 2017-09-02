9cacff4699fcf8515a8c998998cb8702d0858d9f
Ben Wolf <benjamin.wolf.de@googlemail.com>
2017-09-02 19:26:26 +0200

    Adds a GlobalSuppression file
    Microsoft never ceases to amaze me. Apparently you can now add an extra file to store the annotations for all the warnings that you want to disable via comment. Neat.

---
+++ GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Redundancies in Symbol Declarations", "RECS0154:Parameter is never used", Justification = "The method does not do anything at the moment but will need a parameter once it is implemented.", Scope = "member", Target = "~M:CookieJar.Factory.Kitchen.ThrowIntoSink(System.Object)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Redundancies in Symbol Declarations", "RECS0154:Parameter is never used", Justification = "Required for main method. (ノ ゜Д゜)ノ ︵ ┻━┻", Scope = "member", Target = "~M:CookieJar.MainClass.Main(System.String[])")]
+
